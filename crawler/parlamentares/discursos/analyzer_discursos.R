d48fae88d6a8e2c51abc694684d1e101afb3bd60
gileadekelvin <gileadekelvin@gmail.com>
2020-02-22 17:50:52 -0300

    Adiciona crawler de discursos para câmara e senado

---
+++ crawler/parlamentares/discursos/analyzer_discursos.R
@@ -0,0 +1,21 @@
+#' @title Processa dados de discursos da Câmara e do Senado
+#' @description Recupera dados de discursos da Câmara e do Senado na legislatura atual
+#' @return Dataframe contendo informações dos discursos
+#' @examples
+#' processa_dados_discursos()
+processa_dados_discursos <- function() {
+  library(tidyverse)
+  source(here::here("crawler/parlamentares/discursos/fetcher_discursos_camara.R"))
+  source(here::here("crawler/parlamentares/discursos/fetcher_discursos_senado.R"))
+  
+  discursos_camara <- fetch_discursos_todos_deputados()
+  
+  discursos_senado <- fetch_discursos_todos_senadores()
+  
+  discursos <- discursos_camara %>%
+    rbind(discursos_senado) %>% 
+    select(-id_discurso) %>% 
+    tibble::rowid_to_column(var = "id_discurso")
+  
+  return(discursos)
+}
