7cbe980aa25c4adc67ccdaa8b92c435cb8e5834a
Hadrizia <hadrizia@gmail.com>
2020-03-03 18:04:02 -0300

    Adiciona processor de assiuidade

---
+++ bd/processor/assiduidade/processa_assiduidade.R
@@ -0,0 +1,17 @@
+#' @title Processa dados de assiduidade
+#' @description Processa os dados de assiduidade e retorna no formato a ser utilizado pelo banco de dados
+#' @param assiduidade_data_path Caminho para o arquivo de dados de assiduidade
+#' @return Dataframe com informações das assiduidades
+processa_assiduidade <- function(assiduidade_data_path = here::here("crawler/raw_data/assiduidade.csv")) {
+  library(tidyverse)
+  library(here)
+  
+  assiduidade <- read_csv(assiduidade_data_path)
+  
+  assiduidade_alt <-  assiduidade %>%
+    mutate(id_parlamentar_voz = paste0(dplyr::if_else(casa == "camara", 1, 2), 
+                                       id_parlamentar)) %>% 
+    select(id_parlamentar_voz, ano, dias_com_sessoes_deliberativas, dias_presentes, dias_ausencias_justificadas, dias_ausencias_nao_justificadas)
+  
+  return(assiduidade_alt)
+}
--- bd/update_data.R
+++ bd/update_data.R
@@ -119,6 +119,18 @@ tryCatch(
   }
 )
 
+tryCatch(
+  {
+    message(date(), " - Executando crawler de Assiduidade...\n")
+    source(here::here("crawler/parlamentares/assiduidade/export_assiduidade.R"))
+  },
+  error=function(cond) {
+    log_error <- paste(cond, "Um erro ocorreu durante a execução do crawler de Assiduidade")
+    message(log_error)
+    stop("A execução foi interrompida", call. = FALSE)
+    return(NA)
+  }
+)
 
 tryCatch(
   {
