b66aeebeb487ca72fbd401121abfdd0a1c6785f3
gileadekelvin <gileadekelvin@gmail.com>
2020-03-04 18:13:51 -0300

    Refatora função para classificar votações

--- crawler/votacoes/analyzer_votacoes_e_proposicoes_votadas.R
+++ crawler/votacoes/analyzer_votacoes_e_proposicoes_votadas.R
@@ -1,3 +1,43 @@
+#' @title Classifica votações de acordo com seu status e apelido na tabela de votações de interesse do Radar
+#' @description A partir da definição das votações de interesse classifica as votações 
+#' capturadas para a legislatura atual
+#' @param votacoes Dataframe com as votações capturadas 
+#' (precisa pelo menos das colunas id_proposicao, obj_votacao, data, casa)
+#' Data no formato "dd/mm/yyyy"
+#' Hora no formato "mm:ss"
+#' @return Dataframe contendo as votações nominais do período classificadas
+process_votacoes_status_apelido <- function(votacoes) {
+  library(tidyverse)
+  library(here)
+  source(here::here("crawler/votacoes/constants.R"))
+  source(here::here("crawler/utils/utils.R"))
+  
+  votacoes_importantes <- read_csv(.URL_VOTACOES_IMPORTANTES_COPIA) %>% 
+    mutate(id_proposicao = as.character(ID)) %>%
+    mutate(casa = padroniza_nome(Casa) %>% tolower()) %>% 
+    select(nome_proposicao = `Proposição`,
+           id_proposicao,
+           casa,
+           data = `Data da Votação`,
+           objeto_votacao = ObjVotacao,
+           apelido = `Texto do Objeto da Votação Importante`) %>% 
+    mutate(data = ifelse(casa == "camara", 
+                             gsub( " -.*$", "", data),
+                             data
+                             ),
+           data = as.Date(data, format = "%d/%m/%Y"))
+
+  votacoes_alt <- votacoes %>% 
+    mutate(obj_votacao_processed = padroniza_nome(obj_votacao)) %>% 
+    left_join(votacoes_importantes %>% 
+                mutate(objeto_votacao_processed = padroniza_nome(objeto_votacao)), 
+              by = c("id_proposicao", "obj_votacao_processed" = "objeto_votacao_processed", "data", "casa")) %>% 
+    mutate(status_importante = if_else(!is.na(apelido), 1, 0)) %>% 
+    select(-c(nome_proposicao, obj_votacao_processed, objeto_votacao))
+    
+  return(votacoes_alt)
+}
+
 #' @title Lista de votações nominais em um determinado período
 #' @description Retorna um dataframe com as votações de plenário para um determinado período
 #' na Câmara e no Senado
@@ -12,16 +52,6 @@ process_proposicoes_votadas_e_votacoes <-
     source(here::here("crawler/votacoes/fetcher_votacoes_camara.R"))
     source(here::here("crawler/votacoes/fetcher_votacoes_senado.R"))
     source(here::here("crawler/proposicoes/fetcher_proposicoes.R"))
-    source(here::here("crawler/votacoes/constants.R"))
-    source(here::here("crawler/utils/utils.R"))
-    
-    votacoes_importantes <- read_csv(.URL_VOTACOES_IMPORTANTES_COPIA) %>% 
-      mutate(id_proposicao = as.character(ID)) %>% 
-      select(nome_proposicao = `Proposição`,
-             id_proposicao,
-             casa = Casa,
-             objeto_votacao = `Texto do Objeto da Votação Importante`) %>% 
-      filter(!str_detect(objeto_votacao, "sem votações"))
     
     votacoes_camara <-
       fetch_all_votacoes_por_intervalo_camara(initial_date, end_date)
@@ -31,25 +61,20 @@ process_proposicoes_votadas_e_votacoes <-
         votacao_secreta = 0,
         casa = "camara"
       ) %>%
-      mutate(obj_votacao_processed = padroniza_nome(obj_votacao)) %>% 
-      left_join(votacoes_importantes %>% 
-                  mutate(objeto_votacao_processed = padroniza_nome(objeto_votacao)), 
-                by = c("casa", "id_proposicao", "obj_votacao_processed" = "objeto_votacao_processed")) %>% 
-      mutate(status_importante = !is.na(nome_proposicao)) %>% 
-      select(id_proposicao, id_votacao, casa, obj_votacao, data_hora, votacao_secreta, status_importante)
+      process_votacoes_status_apelido() %>% 
+      select(id_proposicao, id_votacao, casa, obj_votacao, data_hora, votacao_secreta, 
+             apelido, status_importante)
     
     votacoes_senado <-
       fetcher_votacoes_por_intervalo_senado(initial_date, end_date)
     votacoes_senado_alt <- votacoes_senado %>%
       mutate(data_hora = as.character(datetime),
              casa = "senado") %>%
-      select(id_proposicao, id_votacao, casa, obj_votacao = objeto_votacao, data_hora,
-             votacao_secreta, url_votacao = link_votacao) %>% 
-      mutate(obj_votacao_processed = padroniza_nome(obj_votacao)) %>% 
-      left_join(votacoes_importantes %>% 
-                  mutate(objeto_votacao_processed = padroniza_nome(objeto_votacao)), 
-                by = c("casa", "id_proposicao", "obj_votacao_processed" = "objeto_votacao_processed")) %>% 
-      mutate(status_importante = !is.na(nome_proposicao))
+      rename(obj_votacao = objeto_votacao,
+             data = datetime) %>% 
+      process_votacoes_status_apelido() %>% 
+      select(id_proposicao, id_votacao, casa, obj_votacao, data_hora,
+             votacao_secreta, apelido, status_importante, url_votacao = link_votacao)
     
     votacoes <- bind_rows(votacoes_camara_alt, votacoes_senado_alt)
     
