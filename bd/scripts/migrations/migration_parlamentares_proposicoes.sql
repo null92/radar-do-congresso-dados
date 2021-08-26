a4091d43266edf5492939bf6490ecf4fcf193746
Lucas Vinícius <lucaspgs2@hotmail.com>
2021-08-26 01:46:30 -0300

    Remove constraint

--- bd/scripts/migrations/migration_parlamentares_proposicoes.sql
+++ bd/scripts/migrations/migration_parlamentares_proposicoes.sql
@@ -7,6 +7,11 @@ CREATE TEMP TABLE temp_parlamentares_proposicoes AS SELECT * FROM parlamentares_
 
 INSERT INTO parlamentares_proposicoes
   SELECT * FROM temp_parlamentares_proposicoes;
+  --ON CONFLICT (id_proposicao_voz, id_parlamentar_voz) 
+  --DO
+    --UPDATE
+    --SET 
+      --ordem_assinatura = EXCLUDED.ordem_assinatura;
 
 DROP TABLE temp_parlamentares_proposicoes;
 
